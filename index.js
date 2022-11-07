119148d3d947637da3a20b3dfc641bfd7b348ce3
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2022-11-06 19:35:18 -0700

    commit tree 20444628260114fba79262b00266b0a79a3ce8e4

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ximena",
+    streetAddress: "2403 E 161st Court"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee};
+    newObj[key] = value
+    return newObj
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee
+}
